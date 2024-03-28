80bf8d149d8f5dc378788ca13f4e07ec1749b71e
Abhijitkadamgit <94745660+Abhijeetkadamgits@users.noreply.github.com>
2024-03-28 15:43:02 +0530

    cng-2

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import { Heading } from "@chakra-ui/react";
+import "../styles/App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CommunityPage } from "../pages/CommunityPage/CommunityPage";
+import { HomePage } from "../pages/HomePage/HomePage";
+import { SubmitPostPage } from "../pages/SubmitPostPage/SubmitPostPage";
+import { EditPostPage } from "../pages/EditPostPage/EditPostPage";
+import { CommentPage } from "../pages/CommentPage/CommentPage";
+import { SearchPage } from "../pages/SearchPage/SearchPage";
+import { RedditPremium } from "../pages/RedditPremium/RedditPremium";
+import { ProfilePage } from "../pages/ProfilePage/ProfilePage";
+import { CommunityNotFound } from "./CommunityPageComponent/CommunityNotFound";
+
+function App() {
+  return (
+    
+    <div >
+       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/community/:channelId" element={<CommunityPage />} />
+        <Route path="/submitpost" element={<SubmitPostPage />} />
+        <Route path="/editpost" element={<EditPostPage />} />
+        <Route path="/comment/:postId" element={<CommentPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/redditpremium" element={<RedditPremium />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+        <Route path="/*" element={<CommunityNotFound />} />
+       </Routes>
+    </div>
+    
+  )
+}
+
+export default App;
---
+++ src/components/utils/projectID.js
@@ -0,0 +1,9 @@
+export const getHeadersWithProjectID = ()=>{
+    const obj = {
+        headers: {
+            projectID: "tp8qxdybuy6b"
+        }
+    }
+
+    return obj;
+}
